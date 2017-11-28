d758858292867dc7c5d2b6d0753c2ccf705f2e7a
Francois BERENGER <ligand@free.fr>
2017-11-28 14:42:23 +0900

    added classify_raw: get the probability of belongin to each class (category) so that users of the library can rank order predictions (instead of just classifying things)

--- src/ORandForest.ml
+++ src/ORandForest.ml
@@ -10,129 +10,154 @@
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************)
 
 module type S = sig
-	type c45_data
-	type c45_category
-	type c45_trainSet
-	type randomForest
+  type c45_data
+  type c45_category
+  type c45_trainSet
+  type randomForest
 
-	val classify : randomForest -> c45_data -> c45_category
-	val genRandomForest : int -> c45_trainSet -> randomForest
+  val classify: randomForest -> c45_data -> c45_category
+  val classify_raw: randomForest -> c45_data -> (c45_category * float) list
+
+  val genRandomForest: int -> c45_trainSet -> randomForest
 end
 
 module Make(X: Oc45.S) = struct
-	(***************** DATA TYPES ********************************************)
-	type c45_data = X.data
-	type c45_category = X.category
-	type c45_trainSet = X.trainSet
+  (***************** DATA TYPES ********************************************)
+  type c45_data = X.data
+  type c45_category = X.category
+  type c45_trainSet = X.trainSet
 
-	module IMap = Map.Make(struct type t=int let compare = compare end)
-	type featureMap = int IMap.t
-	type randomForest = (X.decisionTree * featureMap) array
-	(************* END DATA TYPES ********************************************)
-	open X
+  module IMap = Map.Make(struct type t=int let compare = compare end)
+  type featureMap = int IMap.t
+  type randomForest = (X.decisionTree * featureMap) array
+  (************* END DATA TYPES ********************************************)
+  open X
 
-	let () = Random.self_init ()
+  let () = Random.self_init ()
 
-	let (<|>) a b =
-		(** a|b : generates the list [a ; a+1 ; ... ; b-1] *)
-		let rec span b cur =
-			if a = b then a::cur
-				else span (b-1) (b::cur)
-		in span (b-1) []
+  let (<|>) a b =
+    (** a|b : generates the list [a ; a+1 ; ... ; b-1] *)
+    let rec span b cur =
+      if a = b then a::cur
+      else span (b-1) (b::cur)
+    in span (b-1) []
 
-	let randPick l =
-		let card = List.length l in
-		let elt = Random.int card in
-		List.nth l elt
+  let randPick l =
+    let card = List.length l in
+    let elt = Random.int card in
+    List.nth l elt
 
-	let majorityVote (l : int list) =
-		(** Returns the most present value in l. If the maximum is not unique,
-			returns an arbitrary value among the possible ones. *)
-		let counts = List.fold_left
-			(fun map x -> IMap.add x
-				((try IMap.find x map with Not_found -> 0) + 1) map)
-			IMap.empty l in
-		let cMax,maxarg = IMap.fold (fun arg v (cMax,cArg) ->
-				if v > cMax then
-					(v,[arg])
-				else if v = cMax then
-					(v,arg::cArg)
-				else
-					(cMax,cArg))
-			counts (-1,[]) in
-		assert (maxarg <> []) ;
-		randPick maxarg
+  let majorityVote (l: int list): c45_category =
+    (** Returns the most present value in l. If the maximum is not unique,
+	returns an arbitrary value among the possible ones. *)
+    let counts = List.fold_left
+	(fun map x -> IMap.add x
+	    ((try IMap.find x map with Not_found -> 0) + 1) map)
+	IMap.empty l in
+    let cMax,maxarg = IMap.fold (fun arg v (cMax,cArg) ->
+	if v > cMax then
+	  (v,[arg])
+	else if v = cMax then
+	  (v,arg::cArg)
+	else
+	  (cMax,cArg))
+	counts (-1,[]) in
+    assert (maxarg <> []) ;
+    randPick maxarg
 
-	let remapData featMap data =
-		let out = Array.make (IMap.cardinal featMap) data.(0) in
-		IMap.iter (fun from dest ->
-			out.(dest) <- data.(from)) featMap ;
-		out
+  let vote_frequencies (l: int list): (c45_category * float) list =
+    (** Returns the list of classes (categories) along with
+        the percentage of votes each one got. *)
+    let total = ref 0 in
+    let vote_counts =
+      List.fold_left (fun map categ ->
+          let nb_votes = match IMap.find_opt categ map with
+            | None -> 0
+            | Some n -> n in
+          incr total;
+          IMap.add categ (nb_votes + 1) map
+        ) IMap.empty l in
+    let total_votes = float !total in
+    IMap.fold (fun categ nb_votes acc ->
+        (categ, float nb_votes /. total_votes) :: acc
+      ) vote_counts []
 
-	let classify (forest: randomForest) data =
-		let votesList = Array.fold_left (fun cur (tree,ftMap) ->
-			(X.classify tree (remapData ftMap data))::cur) [] forest in
-		majorityVote votesList
+  let remapData featMap data =
+    let out = Array.make (IMap.cardinal featMap) data.(0) in
+    IMap.iter (fun from dest ->
+	out.(dest) <- data.(from)) featMap ;
+    out
 
-	let genRandomForest nbTrees (trainset : X.trainSet) : randomForest =
-		let trainDataArray = Array.of_list (X.getSet trainset) in
-		let randSubsetOf superSize subSize =
-			let rec sel selected = function
-			| 0 -> selected
-			| k ->
-				let el = Random.int superSize in
-				if IMap.mem el selected then
-					sel selected k
-				else
-					sel (IMap.add el (k-1) selected) (k-1)
-			in
-			sel IMap.empty subSize
-		in
-		let selectFeatureSubset (trList : X.trainVal list) featCont =
-			let subsize = int_of_float (sqrt (float_of_int
-				(X.getNbFeatures trainset))) in
-			let selected = randSubsetOf (X.getNbFeatures trainset)
-				subsize in
-			(List.fold_left (fun cur x ->
-					{ x with data = remapData selected x.data}::cur )
-					[] trList),
-				(remapData selected featCont),
-				selected
-		in
-		let generateTree () =
-			let nTrainList = List.fold_left (fun cur _ ->
-					let sample = Random.int (Array.length trainDataArray) in
-					(trainDataArray.(sample)) :: cur)
-				[] (0<|> (Array.length trainDataArray)) in
-			let trainList, nCont, featMap = selectFeatureSubset nTrainList
-				(X.getFeatContinuity trainset) in
+  let classify' defuzz (forest: randomForest) data =
+    let votesList = Array.fold_left (fun cur (tree,ftMap) ->
+	(X.classify tree (remapData ftMap data))::cur) [] forest in
+    defuzz votesList
 
-			let nTrainSet = List.fold_left (fun cur x -> X.addData x cur)
-				(X.emptyTrainSet
-					(Array.length ((List.hd trainList).data))
-					(X.getNbCategories trainset)
-					nCont)
-				trainList in
-            let ftMaxArray = X.getFeatureMax trainset in
-            IMap.iter (fun ft dest ->
-                    X.setFeatureMax dest ftMaxArray.(ft) nTrainSet) featMap;
-			X.c45 nTrainSet, featMap
-		in
+  let classify =
+    classify' majorityVote
 
-		Array.init nbTrees (fun i -> generateTree ())
-		
-	end
+  let classify_raw =
+    classify' vote_frequencies
+
+  let genRandomForest nbTrees (trainset : X.trainSet) : randomForest =
+    let trainDataArray = Array.of_list (X.getSet trainset) in
+    let randSubsetOf superSize subSize =
+      let rec sel selected = function
+	| 0 -> selected
+	| k ->
+	  let el = Random.int superSize in
+	  if IMap.mem el selected then
+	    sel selected k
+	  else
+	    sel (IMap.add el (k-1) selected) (k-1)
+      in
+      sel IMap.empty subSize
+    in
+    let selectFeatureSubset (trList : X.trainVal list) featCont =
+      let subsize = int_of_float (sqrt (float_of_int
+				          (X.getNbFeatures trainset))) in
+      let selected = randSubsetOf (X.getNbFeatures trainset)
+	  subsize in
+      (List.fold_left (fun cur x ->
+	   { x with data = remapData selected x.data}::cur )
+	  [] trList),
+      (remapData selected featCont),
+      selected
+    in
+    let generateTree () =
+      let nTrainList = List.fold_left (fun cur _ ->
+	  let sample = Random.int (Array.length trainDataArray) in
+	  (trainDataArray.(sample)) :: cur)
+	  [] (0<|> (Array.length trainDataArray)) in
+      let trainList, nCont, featMap = selectFeatureSubset nTrainList
+	  (X.getFeatContinuity trainset) in
+
+      let nTrainSet = List.fold_left (fun cur x -> X.addData x cur)
+	  (X.emptyTrainSet
+	     (Array.length ((List.hd trainList).data))
+	     (X.getNbCategories trainset)
+	     nCont)
+	  trainList in
+      let ftMaxArray = X.getFeatureMax trainset in
+      IMap.iter (fun ft dest ->
+          X.setFeatureMax dest ftMaxArray.(ft) nTrainSet) featMap;
+      X.c45 nTrainSet, featMap
+    in
+
+    Array.init nbTrees (fun i -> generateTree ())
+
+end
 
 module IntRandForest = Make(Oc45.IntOc45)
 module FloatRandForest = Make(Oc45.FloatOc45)
